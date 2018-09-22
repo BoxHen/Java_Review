96151ab1c908e72af0a718773456ce0458a0f77b
Henry <hchen101@binghamton.edu>
2018-09-21 23:33:19 -0400

    buffer

--- common ques/zeroMatrix.java
+++ common ques/zeroMatrix.java
@@ -3,6 +3,50 @@ public class zeroMatrix{
     int[][] matrix = new int[5][5];
   }
   public static void matrix(int[][] matrix, int m, int n){
+    int row[ ]= new int [R];
+            int col[ ]= new int [C];
+            int i, j;
 
+            /* Initialize all values of row[] as 0 */
+            for (i = 0; i < R; i++)
+            {
+            row[i] = 0;
+            }
+
+
+            /* Initialize all values of col[] as 0 */
+            for (i = 0; i < C; i++)
+            {
+            col[i] = 0;
+            }
+
+
+            /* Store the rows and columns to be marked as
+            1 in row[] and col[] arrays respectively */
+            for (i = 0; i < R; i++)
+            {
+                for (j = 0; j < C; j++)
+                {
+                    if (mat[i][j] == 1)
+                    {
+                        row[i] = 1;
+                        col[j] = 1;
+                    }
+                }
+            }
+
+            /* Modify the input matrix mat[] using the
+            above constructed row[] and col[] arrays */
+            for (i = 0; i < R; i++)
+            {
+                for (j = 0; j < C; j++)
+                {
+                    if ( row[i] == 1 || col[j] == 1 )
+                    {
+                        mat[i][j] = 1;
+                    }
+                }
+            }
+        }
   }
 }
