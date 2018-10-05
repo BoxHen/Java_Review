0d9eb5858139c7859ec0d4f38bc5b30ae58b2859
Henry <hchen101@binghamton.edu>
2018-10-05 00:02:23 -0500

    new folder

---
+++ common ques/permutation_palindrome/README.md
@@ -0,0 +1,5 @@
+#Given a string write a function to check if it is a permutation of a palindrome. Does not need to be limited to just dictionary words.
+
+	Example:
+		input: Tact coa
+		output:True (permutations : "taco cat", "atco, cta", etc)
--- common ques/permutation_palindrome/permutation_Palindrome.java
+++ common ques/permutation_palindrome/permutation_Palindrome.java
@@ -1,10 +1,14 @@
+import java.util.Scanner;
 
 public class permutation_Palindrome{
 
   public static void main(String[] args){
-    String input = "tact coaf";
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("enter your text: ");
+    String input = scanner.nextLine(); // String input = "tact coaf";
+    System.out.println("You entered: "+input);
     boolean result = isPermutPalindrome(input);
-    System.out.println(result);
+    //System.out.println(result);
     if(result) System.out.println("This is a palindrome permutation");
     else System.out.println("This is not a palindrome permutation");
   }
@@ -17,9 +21,10 @@ public class permutation_Palindrome{
     for(int i=0; i<str.length(); i++){
       if(str.charAt(i) == ' ') continue; // ignore the spaces
       arr[str.charAt(i)]++;
-      // if we encounter this letter, it will be converted to decimal according to ascii table and we will increment the value
+      // if we encounter this letter, it will be converted to decimal according to ascii table and we will
+      // increment the value
       // this is similar to a hash map where we store a key and value
-      if(arr[str.charAt(i)] % 2 == 0 ){
+      if(arr[str.charAt(i)] % 2 == 0 ){// this makes sense since if a letter count is even, the count will negate it self
         count--; // if num of letter is even
       }else{
         count++; // if num of letter is odd
